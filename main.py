fdc5796479b287b8770112a92abe2caf8540d6c1
Georgi Antonov <georgiantonovv17@gmail.com>
2022-03-22 18:52:50 +0200

    changed the application to flask

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+from flask import Flask
 
-html_text = requests.get('https://www.timeanddate.com/weather/bulgaria/sofia')
+app = Flask(__name__)
 
-soup = BeautifulSoup(html_text.text, 'lxml')
-country = 'Sofia Bulgaria'
-# Here is the forescast for today
-forecast = soup.find('section', class_='bk-focus').span.text
-feels_like = soup.find('section', class_='bk-focus').p.get_text()
 
-print(f'The forecast for two weeks in {country}:')
-for item in soup.find_all('td', class_='wa'):
-      print(f'Forecast for {item.div.text} : {item.p.text}\n')
+@app.route("/result", methods=['GET'])
+def result():
+    html_text = requests.get('https://www.timeanddate.com/weather/bulgaria/sofia')
 
-# print(f'{country} \n'
-#       f'{feels_like}\n'
-#       f'Todays {forecast}')
+    soup = BeautifulSoup(html_text.text, 'lxml')
+    country = 'Sofia Bulgaria'
+
+    forecast = soup.find('section', class_='bk-focus').span.text
+    feels_like = soup.find('section', class_='bk-focus').p.get_text()
+
+    # this will be for two weeks
+    # print(f'The forecast for two weeks in {country}:')
+    # for item in soup.find_all('td', class_='wa'):
+    #     print(f'Forecast for {item.div.text} : {item.p.text}\n')
+
+    answer = (f'{country} \n'
+          f'Todays {forecast}\n'
+          f'{feels_like}')
+
+    return answer
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
